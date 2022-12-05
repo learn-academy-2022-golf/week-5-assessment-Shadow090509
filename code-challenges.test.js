3e60f5fc99b208e2c9e64bf66c7aeb0784a20a59
Shadow090509 <jamoso090509@gmail.com>
2022-12-04 22:55:12 -0800

    push

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,42 +11,149 @@
 // Run the file with the following command: $ yarn jest
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
-
-// a) Create a test with expect statements using the variables provided.
-
-const secretCodeWord1 = "Lackadaisical"
+// My function should be able to take a string and only change the values for the letters a,e,i and o.
+// I moved the jest test up here for functionality
 // Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
 // Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
+// a) Create a test with expect statements using the variables provided.
+// now i show that i can conquer jest
+describe("codeMsg", () => {
+  // I moved the variables ahead of my function in order for there to be no problems of initilization
+  const secretCodeWord1 = "Lackadaisical";
+  const secretCodeWord2 = "Gobbledygook";
+  const secretCodeWord3 = "Eccentric";
+  it('returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.', () => {
+    expect(codeMsg(secretCodeWord1)).toEqual("L4ck4d41s1c4l");
+    expect(codeMsg(secretCodeWord2)).toEqual("G0bbl3dyg00k");
+    expect(codeMsg(secretCodeWord3)).toEqual("3cc3ntr1c");
+  });
+});
+// It's... so... beautiful!
+// this took me a while to figure out. i did some research to see how i can change a specific value to whatever i want and found the .replace(v, v) this does that.
+
+//named my function and new it had to iterate thru a string.
+const codeMsg = (string) => {
+  // I created an empty array to work with
+  let newString = [];
+  // now the hard part.. I knew i need a conditional statement that would change the desired value to what we want, but couldn't figure out an easier way to do this with less statements.. so, i just made one for each instance.
+  for (let i = 0; i < string.length; i++) {
+    //if its an "a" make it a 4 in my new array
+    if (string[i].toLowerCase().includes("a")) {
+      newString.push(string[i].toLowerCase().replace("a", 4));
+      //if its an "e" make it a 3 in my new array
+    } else if (string[i].toLowerCase().includes("e")) {
+      newString.push(string[i].toLowerCase().replace("e", 3));
+      //if its an "i" make it a 1 in my new array
+    } else if (string[i].toLowerCase().includes("i")) {
+      newString.push(string[i].toLowerCase().replace("i", 1));
+      //if its an "o" make it the number 0 in my new array
+    } else if (string[i].toLowerCase().includes("o")) {
+      newString.push(string[i].toLowerCase().replace("o", 0));
+      // And the important one.. if it's not any of the ones i'm looking for, then just add it to the new array unchanged.
+    } else {
+      newString.push(string[i]);
+    }
+  }
+  //now i want to return the array as a "string"
+  return newString.join("");
+};
+// and Bam!
+//console.log(codeMsg("AEIO"));
 
 // b) Create the function that makes the test pass.
+// I also made it to work with upper cased letters.
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
-
+// I think my function will take two arguments and make a new array with only the fruits that contain a specific letter
 // a) Create a test with expects statement using the variable provided.
-
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-
-const letterA = "a"
 // Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
+describe("fruitFunc", () => {
+  const fruitArray = [
+    "Mango",
+    "Cherry",
+    "Apricot",
+    "Blueberry",
+    "Peach",
+    "Kiwi",
+  ];
+  const letterA = "a";
+  const letterE = "e";
+  it("returns an array of all the words containing that particular letter", () => {
+    expect(fruitFunc(fruitArray, letterA)).toEqual([
+      "Mango",
+      "Apricot",
+      "Peach",
+    ]);
+    expect(fruitFunc(fruitArray, letterE)).toEqual([
+      "Cherry",
+      "Blueberry",
+      "Peach",
+    ]);
+  });
+});
+// Look ma! No hands!
+//created the function that takes in two arguments
+const fruitFunc = (array, ltr) => {
+  // created the new array that will be populated with the requested values
+  let newFruitArray = [];
+  for (let i = 0; i < array.length; i++) {
+    // the function will look thru the array and make the value of the current index lowercase in order to check if the value requested is included
+    if (array[i].toLowerCase().includes(ltr)) {
+      // if it is push it to the new array.
+      newFruitArray.push(array[i]);
+    }
+  }
+  // and return the new array
+  return newFruitArray;
+};
+// Bam!
+//console.log(fruitFunc(fruitArray, "m"));
 
 // b) Create the function that makes the test pass.
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
-
+// This sounds like the function should take into accountability all of the values in the array.
 // a) Create a test with expect statements using the variable provided.
-
-const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
+describe("dealer", () => {
+  const hand1 = [5, 5, 5, 3, 3];
+  const hand2 = [5, 5, 3, 3, 4];
+  const hand3 = [5, 5, 5, 5, 4];
+  const hand4 = [7, 2, 7, 2, 7];
+  it("determines whether or not the array is a full house", () => {
+    expect(dealer(hand1)).toEqual(true);
+    expect(dealer(hand2)).toEqual(false);
+    expect(dealer(hand3)).toEqual(false);
+    expect(dealer(hand4)).toEqual(true);
+  });
+});
+// looks good! all three tests ran and passed succesfully!
+// named the function dealer, because its going to do the job of a dealer.
+const dealer = (array) => {
+  // created a count to keep track of how many times a value repeats in an array.
+  let count1 = {};
+  // used a For of loop in order to check the value and keep track of how many times it is used
+  for (let x of array) {
+    count1[x] = (count1[x] || 0) + 1;
+  }
+  // I am using the Object.values method to look into the count1 we created and see if there is 3 of a kind and a pair that was tracked.
+  let count = Object.values(count1);
+  if (
+    (count[0] === 2 && count[1] === 3) ||
+    (count[1] === 2 && count[0] === 3)
+  ) {
+    // If there is it returns true
+    return true;
+  } else {
+    return false;
+  }
+};
+// and Bam!
+//console.log(dealer([1, 2, 2, 1, 2]));
 
 // b) Create the function that makes the test pass.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -2,29 +2,100 @@
 # MINASWAN
 
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
-
+# Okay, I must take this array and return an array with the words that contain the letter of my choosing
 beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
-
-
+# the method must take two arguments
+def bev_array(array, ltr)
+  # I use the select method to make a new array with the word that includes the letter. can't forget the pipes
+  array.select { |word| word.include? ltr}
+end
+p bev_array(beverages_array, letter_o)
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
+# googled it. the second result was named folks talk and was to the point about the methods that can be used to put everything onto one array
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], northeast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+# made the method that takes the hash as the argument
+def alph_states(hash)
+  # I want to get the values onto one array and sort them alphabetically. i understood flatten as a way to put them on the same plane or array
+   hash.values.flatten.sort
+end
+p alph_states(us_states)
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
+class Bike
+# Not sure if this top part is needed, but i was just following the syllabus
+  attr_accessor :model, :wheels, :current_speed
+  
+  def initialize(model)
+  @model = model 
+  @wheels = 2
+  @current_speed = 0
+  end
+# getter method
+  def get_wheels
+    @wheels
+  end
+# made the model customizable
+  def set_model(model)
+    @model = model
+  end
+# getter method
+  def get_model
+    @model
+  end
+# -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
+# acceleration
+  def pedal_faster(num)
+    @current_speed += (num)
+  end
+# decceleration
+  def brake(num)
+    @current_speed -= (num)
+    #Forgot we can put conditionals inside of a method. thats how i figured out ho to stop the speed from going below zero
+    if @current_speed <= 0
+      @current_speed = 0
+
+      
+    end
+  end
+# getter method
+  def get_speed
+    @current_speed
+  end
+#getter method
+  def get_info
+    "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph"
+  end
+
+
+end
+# gave the bike a creative name
+bike = Bike.new('bike')
+# made it an exotic model
+bike.set_model('Honda')
+# pushed it to the limits
+bike.pedal_faster(100)
+# saw a cop and had to hit the brakes hard!
+bike.brake(150)
+
+
+# checked to see how fast I'm going
+p bike.get_info
+
+
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 
 
-# -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
